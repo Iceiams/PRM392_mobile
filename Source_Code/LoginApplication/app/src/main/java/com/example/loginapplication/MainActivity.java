bc11c9b32499d9f447a6330298e5c5cb88aa8736
Mai Tran <maimaimaihoabinh@gmail.com>
2024-05-20 17:18:12 +0700

    Handling_action_login mtran

--- Source_Code/LoginApplication/app/src/main/java/com/example/loginapplication/MainActivity.java
+++ Source_Code/LoginApplication/app/src/main/java/com/example/loginapplication/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.loginapplication;
 
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class MainActivity extends AppCompatActivity {
+    public Button myButton;
+    public EditText myUser;
+    public EditText myPass;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,20 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        myButton = findViewById(R.id.loginbtn);
+        myUser = findViewById(R.id.username);
+        myPass = findViewById(R.id.password);
+
     }
+    public void onLogin(View view){
+        if (myUser.getText().toString().equals("admin") &&
+                myPass.getText().toString().equals("12345"))
+        {
+            Toast.makeText(getApplicationContext(),"Login successful",Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(),"Login failed",Toast.LENGTH_SHORT).show();
+        }
+        }
 }
