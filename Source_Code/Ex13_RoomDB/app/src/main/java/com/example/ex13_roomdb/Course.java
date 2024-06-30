1710ddf4ec1ea03afc019aadaa84fd09c3356f01
Mai Tran <maimaimaihoabinh@gmail.com>
2024-06-30 23:51:59 +0700

    Ex13_roomDB mtran

---
+++ Source_Code/Ex13_RoomDB/app/src/main/java/com/example/ex13_roomdb/Course.java
@@ -0,0 +1,42 @@
+package com.example.ex13_roomdb;
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+@Entity(tableName = "courses") // Set Table name
+public class Course {
+    // Table fields: id (Primary key - NonNull), name, description
+    @PrimaryKey
+    @NonNull
+    private String id;
+    private String name;
+    private String description;
+    public Course(@NonNull String id, String name, String description)
+    {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+    // com.example.ex13_roomdb.Course ID
+    @NonNull
+    public String getId() {
+        return id;
+    }
+    public void setId(@NonNull String id) {
+        this.id = id;
+    }
+    // Name
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    // Description
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
